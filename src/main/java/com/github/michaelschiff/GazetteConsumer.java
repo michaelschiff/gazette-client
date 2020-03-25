65acec45b6f3708d847f9b768dfca085ab9bbab6
Michael Schiff <schiff.michael@gmail.com>
2020-03-24 20:23:23 -0700

    able to make basic calls to the broker via Journal service

--- src/main/java/com/github/michaelschiff/GazetteConsumer.java
+++ src/main/java/com/github/michaelschiff/GazetteConsumer.java
@@ -1,11 +1,27 @@
 package com.github.michaelschiff;
 
+import dev.gazette.core.broker.protocol.JournalGrpc;
+import dev.gazette.core.broker.protocol.Protocol;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class GazetteConsumer {
 
-
-
-
     public static void main(String[] args) {
-
+        ManagedChannelBuilder<?> builder = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext();
+        ManagedChannel channel = builder.build();
+        JournalGrpc.JournalBlockingStub stub = JournalGrpc.newBlockingStub(channel);
+        Protocol.LabelSelector labelSelector = Protocol.LabelSelector.newBuilder()
+                .setInclude(Protocol.LabelSet.newBuilder()
+                        .addLabels(Protocol.Label.newBuilder()
+                                .setName("prefix")
+                                .setValue("test/")
+                                .build()))
+                .build();
+        Protocol.ListRequest listReq = Protocol.ListRequest.newBuilder()
+                .setSelector(labelSelector)
+                .build();
+        Protocol.ListResponse list = stub.list(listReq);
+        System.out.println(list);
     }
 }
