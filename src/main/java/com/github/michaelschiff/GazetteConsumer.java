0dac9e675bac0c1b0d21a3038b6e2cc5bb80d3f1
Michael Schiff <schiff.michael@gmail.com>
2020-03-24 21:18:29 -0700

    stream example

--- src/main/java/com/github/michaelschiff/GazetteConsumer.java
+++ src/main/java/com/github/michaelschiff/GazetteConsumer.java
@@ -2,26 +2,40 @@ package com.github.michaelschiff;
 
 import dev.gazette.core.broker.protocol.JournalGrpc;
 import dev.gazette.core.broker.protocol.Protocol;
+import dev.gazette.core.broker.protocol.Protocol.ReadResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
 
 public class GazetteConsumer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ManagedChannelBuilder<?> builder = ManagedChannelBuilder.forAddress("localhost", 8080).usePlaintext();
         ManagedChannel channel = builder.build();
-        JournalGrpc.JournalBlockingStub stub = JournalGrpc.newBlockingStub(channel);
-        Protocol.LabelSelector labelSelector = Protocol.LabelSelector.newBuilder()
-                .setInclude(Protocol.LabelSet.newBuilder()
-                        .addLabels(Protocol.Label.newBuilder()
-                                .setName("prefix")
-                                .setValue("test/")
-                                .build()))
+        JournalGrpc.JournalStub stub = JournalGrpc.newStub(channel);
+        Protocol.ReadRequest readReq = Protocol.ReadRequest.newBuilder()
+                .setBlock(true)
+                .setOffset(0)
+                .setJournal("test/part-002")
                 .build();
-        Protocol.ListRequest listReq = Protocol.ListRequest.newBuilder()
-                .setSelector(labelSelector)
-                .build();
-        Protocol.ListResponse list = stub.list(listReq);
-        System.out.println(list);
+        StreamObserver<ReadResponse> observer = new StreamObserver<ReadResponse>() {
+
+            @Override
+            public void onNext(Protocol.ReadResponse readResponse) {
+                System.out.println(readResponse);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+        stub.read(readReq, observer);
+        Thread.sleep(30000);
     }
 }
